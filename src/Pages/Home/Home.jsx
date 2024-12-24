11dfb5aa09916e782849c206f03260a5fefd1cf8
bijoymamud <bijoymamud.09@gmail.com>
2024-12-24 15:31:24 +0600

    initialize the project and setup one template

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import Container from "../../Layout/Container/Container"
+import TemplateOne from "../FirstOneTemplate/TemplateOne"
+
+const Home = () => {
+  return (
+    <div>
+          <Container>
+          <TemplateOne/>
+      </Container>
+    </div>
+  )
+}
+
+export default Home
